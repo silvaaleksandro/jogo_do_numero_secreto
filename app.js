39af4bc1790ff56619421164ba5aae1d221bdcc5
silvaaleksandro <sivlaal@gmail.com>
2025-08-18 16:35:37 -0300

    Adicionada console.log na linha 6 do app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+console.log("teste alteração via git no CLI")
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
