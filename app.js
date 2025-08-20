5fbd37402c2fe41da43161a242713b671ba0db3b
guinhogun <contaestudo01012000@gmail.com>
2025-08-20 10:56:06 -0300

    limite 20

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 60;
+let numeroLimite = 20;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 60');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 exibirMensagemInicial();
